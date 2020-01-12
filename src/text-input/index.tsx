c2b341334be3b7642efa587e24e92ba00858c972
xiaohulu <idocument@qq.com>
2020-01-12 20:40:13 +0800

    将 TextInput 转换为基于函数的部件 (#4)

--- src/text-input/index.tsx
+++ src/text-input/index.tsx
@@ -1,5 +1,5 @@
 import WidgetBase from "@dojo/framework/core/WidgetBase";
-import { tsx } from "@dojo/framework/core/vdom";
+import { create, tsx } from "@dojo/framework/core/vdom";
 import * as css from "./index.m.css";
 import * as c from "bootstrap-classes";
 
@@ -10,21 +10,20 @@ export interface TextInputProperties {
 	onValue?(value?: string): void;
 }
 
-export default class TextInput extends WidgetBase<TextInputProperties> {
-	protected render() {
-		const { value = "", onValue } = this.properties;
+const factory = create().properties<TextInputProperties>();
 
-		return (
-			<input
-				key="root"
-				classes={[css.root, c.form_control]}
-				value={value}
-				oninput={(event: Event) => {
-					event.stopPropagation();
-					const value = (event.target as HTMLInputElement).value;
-					onValue && onValue(value);
-				}}
-			/>
-		);
-	}
-}
+export default factory(function TextInput({ properties }) {
+	const { value = "", onValue } = properties();
+	return (
+		<input
+			key="root"
+			classes={[css.root, c.form_control]}
+			value={value}
+			oninput={(event: Event) => {
+				event.stopPropagation();
+				const value = (event.target as HTMLInputElement).value;
+				onValue && onValue(value);
+			}}
+		/>
+	);
+});
--- tests/unit/text-input/index.tsx
+++ tests/unit/text-input/index.tsx
@@ -7,16 +7,22 @@ import { stub } from "sinon";
 import TextInput from "../../../src/text-input";
 import * as c from "bootstrap-classes";
 import * as css from "../../../src/text-input/index.m.css";
+import assertionTemplate from "@dojo/framework/testing/assertionTemplate";
+
+const baseAssertion = assertionTemplate(() => (
+	<input key="root" classes={[css.root, c.form_control]} value="" oninput={() => {}} />
+));
 
 describe("TextInput", () => {
 	it("default properties", () => {
 		const h = harness(() => <TextInput />);
-		h.expect(() => <input key="root" classes={[css.root, c.form_control]} value="" oninput={() => {}} />);
+		h.expect(baseAssertion);
 	});
 
 	it("custom properties", () => {
+		const valueAssertion = baseAssertion.setProperty("@root", "value", "1");
 		const h = harness(() => <TextInput value="1" />);
-		h.expect(() => <input key="root" classes={[css.root, c.form_control]} value="1" oninput={() => {}} />);
+		h.expect(valueAssertion);
 	});
 
 	it("onValue", () => {
