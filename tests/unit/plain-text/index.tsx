c3f142d86d4a96520679d5cbb469ec52c71ca6e9
xiaohulu <idocument@qq.com>
2020-01-12 20:24:25 +0800

    测试用例改为使用 assertionTemplate

--- tests/unit/plain-text/index.tsx
+++ tests/unit/plain-text/index.tsx
@@ -3,15 +3,19 @@ const { describe, it } = intern.getInterface("bdd");
 import harness from "@dojo/framework/testing/harness";
 import { tsx } from "@dojo/framework/core/vdom";
 import PlainText from "../../../src/plain-text";
+import assertionTemplate from "@dojo/framework/testing/assertionTemplate";
+
+const baseAssertion = assertionTemplate(() => <virtual key="root"></virtual>);
 
 describe("PlainText", () => {
 	it("default properties", () => {
 		const h = harness(() => <PlainText />);
-		h.expect(() => <virtual key="root"></virtual>);
+		h.expect(baseAssertion);
 	});
 
 	it("custom properties", () => {
+		const valueAssertion = baseAssertion.setChildren("@root", () => ["Hello"]);
 		const h = harness(() => <PlainText value="Hello" />);
-		h.expect(() => <virtual key="root">Hello</virtual>);
+		h.expect(valueAssertion);
 	});
 });
